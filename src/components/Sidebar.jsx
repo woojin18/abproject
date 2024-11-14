c89f4e46a0b01881cc36829aef14887797100750
woojin <woojin180213@naver.com>
2024-11-14 23:32:47 +0900

    프리티어 설정

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,6 @@
 import React from "react";
-import {
-  Drawer,
-  List,
-  ListItem,
-  ListItemText,
-  ListItemIcon,
-  Divider,
-  Typography,
-  Box,
-} from "@mui/material";
+import { Drawer, List, ListItem, ListItemText, ListItemIcon, Divider, Typography, Box, ListItemButton } from "@mui/material";
+
 import { Home, Receipt, BarChart, Settings } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 
@@ -18,60 +10,48 @@ function Sidebar({ isDrawerOpen, toggleDrawer }) {
       anchor="left"
       open={isDrawerOpen}
       onClose={toggleDrawer}
-      sx={{
-        "& .MuiDrawer-paper": {
-          width: 240, 
-          backgroundColor: "#f4f4f4", 
-          color: "#333",
-        },
-      }}
+      sx={{ "& .MuiDrawer-paper": { width: 240, backgroundColor: "#f4f4f4", color: "#333" } }}
     >
-      <Box
-        sx={{
-          padding: "16px",
-          textAlign: "center",
-          backgroundColor: "#1976d2",
-          color: "white",
-        }}
-      >
+      <Box sx={{ padding: "16px", textAlign: "center", backgroundColor: "#1976d2", color: "white" }}>
         <Typography variant="h6">Budget App</Typography>
       </Box>
       <Divider />
 
       <List>
-        <ListItem button component={Link} to="/" onClick={toggleDrawer}>
-          <ListItemIcon>
-            <Home sx={{ color: "#1976d2" }} />
-          </ListItemIcon>
-          <ListItemText primary="Home" />
+        <ListItem disablePadding>
+          <ListItemButton component={Link} to="/" onClick={toggleDrawer}>
+            <ListItemIcon>
+              <Home sx={{ color: "#1976d2" }} />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItemButton>
         </ListItem>
-        <ListItem
-          button
-          component={Link}
-          to="/transactions"
-          onClick={toggleDrawer}
-        >
-          <ListItemIcon>
-            <Receipt sx={{ color: "#1976d2" }} />
-          </ListItemIcon>
-          <ListItemText primary="Transactions" />
+
+        <ListItem disablePadding>
+          <ListItemButton component={Link} to="/transactions" onClick={toggleDrawer}>
+            <ListItemIcon>
+              <Receipt sx={{ color: "#1976d2" }} />
+            </ListItemIcon>
+            <ListItemText primary="Transactions" />
+          </ListItemButton>
         </ListItem>
-        <ListItem
-          button
-          component={Link}
-          to="/statistics"
-          onClick={toggleDrawer}
-        >
-          <ListItemIcon>
-            <BarChart sx={{ color: "#1976d2" }} />
-          </ListItemIcon>
-          <ListItemText primary="Statistics" />
+
+        <ListItem disablePadding>
+          <ListItemButton component={Link} to="/statistics" onClick={toggleDrawer}>
+            <ListItemIcon>
+              <BarChart sx={{ color: "#1976d2" }} />
+            </ListItemIcon>
+            <ListItemText primary="Statistics" />
+          </ListItemButton>
         </ListItem>
-        <ListItem button component={Link} to="/settings" onClick={toggleDrawer}>
-          <ListItemIcon>
-            <Settings sx={{ color: "#1976d2" }} />
-          </ListItemIcon>
-          <ListItemText primary="Settings" />
+
+        <ListItem disablePadding>
+          <ListItemButton component={Link} to="/settings" onClick={toggleDrawer}>
+            <ListItemIcon>
+              <Settings sx={{ color: "#1976d2" }} />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItemButton>
         </ListItem>
       </List>
     </Drawer>
